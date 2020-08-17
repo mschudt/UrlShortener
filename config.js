82d7705cef7d87dc06bead4ce5197784ae3e91cd
Markus S <markus>
2020-08-17 03:38:36 +0200

    Removed default hostname, must be passed as a cli arg

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
-// hostname and port can be passed as cli args
-// default is set to the ip address of the eth0 network interface
-let HOSTNAME = process.argv[2] || require("os").networkInterfaces().eth0[0].address;
+// hostname must be passed as the first commandline argument
+let HOSTNAME = process.argv[2];
+if (HOSTNAME === undefined) {
+    console.error("The first argument passed must be a valid hostname. Like: https://example.com");
+    process.exit();
+}
+
 // append "https://" to the start of the hostname if it's missing
 HOSTNAME = HOSTNAME.startsWith("https://") ? HOSTNAME : "https://" + HOSTNAME;
 // append "/" if it is missing
 HOSTNAME = HOSTNAME.endsWith("/") ? HOSTNAME : HOSTNAME + "/";
 
+// port can be passed as the second cli arg
 const PORT = process.argv[3] || "3001";
 
 // parameter length of the shortened url
