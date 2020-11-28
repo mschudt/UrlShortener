ee35fb942d88209726c77142f864e70dd3d8a699
Markus S <markus>
2020-11-28 18:59:36 +0100

    better print message

--- src/main/index.js
+++ src/main/index.js
@@ -121,7 +121,7 @@ app.get("/text/:id", (req, res) => {
 
 app.get("/text", (req, res) => {
     textUploaderVisitsCounter += 1;
-    console.log(urlShortenerVisitsCounter);
+    console.log("/text visits: " + urlShortenerVisitsCounter);
 
     res.set("Content-Type", "text/html");
 
@@ -196,7 +196,7 @@ app.get("/:id/raw", (req, res) => {
 // Show start page if no url id was passed
 app.get("/", (req, res) => {
     urlShortenerVisitsCounter += 1;
-    console.log(urlShortenerVisitsCounter);
+    console.log("/ visits: " + urlShortenerVisitsCounter);
 
     res.set("Content-Type", "text/html");
     res.send(HtmlRenderer.renderStartingPage());
