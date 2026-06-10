dcc1c3c1385a7deea64b42a13045aa8c30cd04d7
mschudt <markus.schudt@gmail.com>
2026-06-10 20:14:40 +0200

    Added reserved words that should not be selected

--- src/main/util.js
+++ src/main/util.js
@@ -1,7 +1,25 @@
 const randomWords = require("random-words");
 
+// Current and known route/static path segments that must not be generated as URL ids.
+const reservedShortUrlIds = new Set([
+    "create",
+    "favicon",
+    "raw",
+    "text",
+    "use",
+]);
+
+export const isReservedShortUrlId = (id) => {
+    return reservedShortUrlIds.has(id);
+}
+
 export const generateNewIdAsRandomWord = (length) => {
-    return randomWords({exactly: 1, maxLength: length});
+    let randomId;
+    do {
+        randomId = randomWords({exactly: 1, maxLength: length})[0];
+    } while (isReservedShortUrlId(randomId));
+
+    return randomId;
 }
 
 export const StoredType = Object.freeze({"URL": 0, "Text": 1});
